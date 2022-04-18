1aa1b23e2debc311099607921c0e15b4ee248a44
Joey Lundquist <joeylundquist91@gmail.com>
2022-04-18 13:28:25 -0500

    Finished the first 2 strectch goals

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -87,22 +87,36 @@ console.log(find(55, testNumbers));
 // 8. Function to check if a letter is the first letter in a 
 //    string. Return true if it is, and false otherwise
 function isFirstLetter(letter, string) {
-    
+  if(letter === string.charAt(0)){
+    return true;
+  }
+  return false;
 }
 console.log('isFirstLetter - should say true', isFirstLetter('a', 'apple'));
 console.log('isFirstLetter - should say false', isFirstLetter('z', 'apple'));
 
 // 9. Function to return the sum of all numbers in an array
-function sumAll(){
+let addAll = [2, 4, 6, 8, 10]
+let addAll2 = [5, 6, 10, 33, -27, 123, 321, 900, -99]
+function sumAll(array){
   let sum = 0
   // TODO: loop to add items
+  for(i=0; i<=array.length-1; i++){
+    sum += array[i];
+  }
   return sum;
 }
+console.log(sumAll(testNumbers));
+console.log(sumAll(addAll));
+console.log(sumAll(addAll2));
 
 // 10. Function to return a new array of all positive (greater than zero)
 //     numbers contained in an input array. If there are no positive numbers
 //     return an empty array. Note: The input array should not change.
-
+let num10 = [1, 2, 3, -3, -2, -1]
+function trackPositive(array){
+  
+}
 
 
 // 11. Pick a problem from Edabit(https://edabit.com/) or 
